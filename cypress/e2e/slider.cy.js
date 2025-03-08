093fc499469d3af240325e20dd12e87dbdac8c7c
Jakub <jakub180720@gmail.com>
2025-03-08 18:08:09 +0100

    added test and hotjar script

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,24 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Slide Content Test', function () {
+  it('Verifies that each slide has correct title and description', function () {
+    cy.visit('http://localhost:3000');
+    
+    const slides = [
+      { title: 'Rome', description: 'Italy' },
+      { title: 'London', description: 'United Kingdom' },
+      { title: 'Paris', description: 'France' }
+    ];
+    
+    slides.forEach((slide, index) => {
+      if (index > 0) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(1000);
+      }
+      cy.get('.swiper-slide-active').should('contain', slide.title);
+      cy.get('.swiper-slide-active').should('contain', slide.description);
+    });
+  });
+});
